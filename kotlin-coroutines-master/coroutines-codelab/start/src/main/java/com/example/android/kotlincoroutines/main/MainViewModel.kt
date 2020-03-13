374957d86af24ca4075b1f99c1ad632725cb986a
Adr1anZarza <9055350+adr1anzarza@users.noreply.github.com>
2020-03-13 11:22:06 -0600

    Starter code added

--- kotlin-coroutines-master/coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ kotlin-coroutines-master/coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.kotlincoroutines.main
 
 import androidx.lifecycle.LiveData
@@ -101,7 +85,7 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Wait one second then update the tap count.
      */
     private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
+        // TODO 01: Convert updateTaps to use coroutines
         tapCount++
         BACKGROUND.submit {
             Thread.sleep(1_000)
